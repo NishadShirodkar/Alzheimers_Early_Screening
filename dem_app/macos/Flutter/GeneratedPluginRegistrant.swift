a8d06f2dfc55d1b34b65617003bb007350e185d3
SaurabhShahane30 <saur30805@gmail.com>
2026-04-03 23:14:54 +0530

    mmse changed

--- dem_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dem_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_darwin
+import flutter_tts
+import geolocator_apple
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
