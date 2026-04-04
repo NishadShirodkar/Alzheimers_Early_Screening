0e2ee66bf95f3d90adaa150aec63d413650e9abd
SaurabhShahane30 <saur30805@gmail.com>
2026-04-05 00:37:27 +0530

    twilo integration completed

--- dem_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dem_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import flutter_secure_storage_darwin
 import flutter_sound
 import flutter_tts
@@ -13,6 +14,7 @@ import record_macos
 import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FlutterSoundPlugin.register(with: registry.registrar(forPlugin: "FlutterSoundPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
